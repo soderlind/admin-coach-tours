f64659ef6e4b8c53a0cfb35cf559808a6e25b168
Per Søderlind <per@soderlind.no>
2026-02-02 21:58:58 +0100

    Refactor code structure for improved readability and maintainability
    - Organized the project directory by grouping related files into subfolders.
    - Renamed several functions and variables for clarity, ensuring they accurately reflect their purpose.
    - Removed redundant code snippets to streamline functionality and reduce complexity.
    - Updated comments and documentation to provide clearer guidance on code usage and logic.
    - Enhanced error handling in critical sections to improve robustness and user experience.

--- build/pupil/index.asset.php
+++ build/pupil/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0c1409e60c635f35cca0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c1ca2a44ce68c0948266');
